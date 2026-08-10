a428fc5d27a6a9ea7ca83a09a6f9a97990145515
churumaahh <hiocoffee0823@gmail.com>
2026-08-10 11:23:30 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 ﻿window.CLOVER_CONFIG = Object.freeze({
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzOMZ0MH2p3uzCNeFk59OeHqqTxgc7ay0zYYZmRsqbzOGNCHpUyaS-d7lOtUf-UjIXO/exec"
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwOkUM3C0qOLvEyoexGzahBWpHdEMHIyxWLnW4bLTzQROBV7N_tFtD3Ys2mzp6EO2la/exec"
 });
 
 
